b4f7d08ec3bad0e969d9fc3a5a926b10779e2e8a
FARABI12344 <farabiehan4@gmail.com>
2025-05-03 10:49:01 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        'fs', 'path', 'os', 'events', 'child_process',
-        'net', 'tls', 'assert', 'stream', 'zlib',
-        'module', 'http', 'https', 'url', 'fsevents',
-        'crypto', 'querystring'
-      ]
+      '@': '/src'
     }
   }
 })
